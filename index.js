2f64cfcd385df62bd5ad64b197df006fda47803e
Md Noble <mdnoble.wd@gmail.com>
2023-12-07 17:56:33 +0600

    created api to check isAdmin

--- index.js
+++ index.js
@@ -48,6 +48,31 @@ async function run() {
       res.send({ token });
     })
 
+    // middlewares to verify token 
+    const verifyToken = (req, res, next) => {
+      console.log('inside verify token', req.headers.authorization);
+
+      if(!req.headers.authorization){
+        return res.status(401).send({message: "unauthorized access"})
+      }
+
+      const token = req.headers.authorization.split(' ')[1];
+      // if(!token){
+      //   // can send a status and message like in the top 
+      // }
+
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if(err){
+          return res.status(401).send({message: "unauthorized access"})
+        }
+
+        req.decoded = decoded;
+        next();
+      })
+
+      // next();
+    }
+
 
 
     // get all menu from database 
@@ -63,7 +88,9 @@ async function run() {
     })
 
     // get all users from database 
-    app.get('/users', async(req, res) => {
+    app.get('/users', verifyToken, async(req, res) => {
+      // console.log(req.headers);
+
       const result = await userCollection.find().toArray();
       res.send(result);
     })
@@ -75,6 +102,25 @@ async function run() {
       const query = { email : email }
       const result = await cartCollection.find(query).toArray();
       res.send(result);
+    })
+
+    //check for users that are admin or not 
+    app.get('/users/admin/:email', verifyToken, async(req, res) => {
+      const email = req.params.email;
+
+      if( email !== req.decoded.email){
+        return res.status(403).send({message: 'forbidden access'})
+      }
+
+      const query = {email: email};
+      const user = await userCollection.findOne(query);
+      
+      let admin = false;
+      if(user){
+        admin = user?.role === 'admin'; 
+      }
+
+      res.send({ admin });
     })
 
 
